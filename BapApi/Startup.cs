e920172f0dde614e5ce7679714b5fbc459ea305c
Kelvin Mbewe <kisskeo@gmail.com>
2021-06-25 23:12:13 +0100

    add comments to startup.cs

--- BapApi/Startup.cs
+++ BapApi/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
 using BapApi.Models;
 
 namespace BapApi
@@ -17,13 +16,26 @@ namespace BapApi
 
         public IConfiguration Configuration { get; }
 
+        /// <summary>
+        /// The ServiceCollection Dependency Injection (DI) is supported and applied for BAP services. 
+        /// This is straight as we just add it to the IServiceCollection in the ConfigureServices() 
+        /// pipeline.
+        /// https://tim-maes.com/2019/10/21/net-core-tutorial-using-the-servicecollection-extension-pattern/
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<StoreAppsContext>(opt => opt.UseInMemoryDatabase("StoreApps"));
             services.AddEntityFrameworkSqlite().AddDbContext<StoreAppsContext>();
             services.AddControllers();
         }
 
+        /// <summary>
+        /// IApplicationBuilder Provides help configure and start web applications pages.
+        /// https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.hosting?view=aspnetcore-5.0
+        /// https://andrewlock.net/comparing-startup-between-the-asp-net-core-3-templates/
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -33,9 +45,7 @@ namespace BapApi
 
             app.UseHttpsRedirection();
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
