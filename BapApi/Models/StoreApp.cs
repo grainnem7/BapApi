29b12cdc658ed19a3a55cb6a80ff34805f2a2852
Kelvin Mbewe <kisskeo@gmail.com>
2021-06-26 01:12:26 +0100

    update validation model to storeApp.cs

--- BapApi/Models/StoreApp.cs
+++ BapApi/Models/StoreApp.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 /// <summary>
 /// We will validate our API model for security reasons because insertion data from the frontend will be saved in our database.
@@ -6,31 +8,51 @@ using System.ComponentModel.DataAnnotations;
 /// For example, if there is a form in the frrontend application and the frrontend developer forgets to validate input fields,
 /// the result can affect the model validation stage. You may even implement input validation at the DB level too.
 /// https://www.c-sharpcorner.com/UploadFile/dacca2/model-validation-in-web-api/
+/// https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-aspnet-mvc3/cs/adding-validation-to-the-model
+/// https://entityframework.net/database-generated#:~:text=Identity.%20When%20DatabaseGenerated.Identity%20applied%20to%20a%20property%2C%20the,by%20the%20database%20when%20the%20row%20is%20inserted.
 /// </summary>
 namespace BapApi.Models
 {
     public class StoreApp
     {
-        [Required]
-        public int    Id       { get; set; }
+        [Column("id")]
+        [Display(Name = "App ID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "ID is required")]
+        public int Id { get; set; }
 
-        [StringLength(100, MinimumLength = 3), Required]
-        public string Name     { get; set; }
+        [Column("name")]
+        [Display(Name = "App Name")]
+        [StringLength(100, MinimumLength = 3)]
+        [Required(ErrorMessage = "App name is required")]
+        public string Name { get; set; }
 
-        [Range(0, 4)]
-        public double Rating   { get; set; }
-
+        [Column("rating")]
+        [Display(Name = "Rating")]
+        [StringLength(5)]
         [RegularExpression("^[0-9]*$")]
-        public int    People   { get; set; }
+        public double Rating { get; set; }
 
-        [Required]
+        [Column("people")]
+        [Display(Name = "Number of People Downloaded")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int People { get; set; }
+
+        [Column("category")]
+        [Display(Name = "Category")]
+        [Required(ErrorMessage = "Category is required")]
         public string Category { get; set; }
 
-        [Required]
-        public string Date     { get; set; }
+        [Column("date")]
+        [Display(Name = "Date")]
+        [Required(ErrorMessage = "Date is required")]
+        public string Date { get; set; }
 
-        [Required]
-        public string Price    { get; set; }
+        [Column("price")]
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "Price is required")]
+        public string Price { get; set; }
     }
 
     /// <summary>
